90bc76f5e9588e9d4119e73ce5597d9d7912d32b
IsraelTorrez <141113013+IsraelTorrez@users.noreply.github.com>
2023-10-10 02:31:13 +0000

    live

---
+++ index.js
@@ -0,0 +1,25 @@
+
+/*
+Varias funciones en JS
+*/
+
+function suma(a, b) {
+    return a + b
+}
+
+const resta = function(a, b) {
+    return a - b
+}
+
+const producto = (a, b) => a * b
+
+const nombreCompleto = (nombre) => `Hola, cómo estas ${nombre}`
+
+const resultado_suma = suma(2, 8)
+const resultado_resta = resta(1, 5)
+const resultado_producto = producto(3, 6)
+
+console.log(resultado_suma)
+console.log(resultado_resta)
+console.log(resultado_producto)
+console.log(nombreCompleto('Carlos'));
